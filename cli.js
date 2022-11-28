65d4fa9531d257e1dd996f960e8d4b2c5a758674
Arvind Veerelli <veerelliarvind@unc.edu>
2022-11-27 23:50:50 -0500

    a02 done

---
+++ cli.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+
+import minimist from 'minimist';
+import moment from 'moment-timezone';
+import fetch from 'node-fetch';
+
+const args = minimist(process.argv.slice(2));
+
+if(args.h) {
+    console.log('Usage: galosh.js [options] -[n|s] LATITUDE -[e|w] LONGITUDE -z TIME_ZONE');
+    console.log('-h            Show this help message and exit.');
+    console.log('-n, -s        Latitude: N positive; S negative.');
+    console.log('-e, -w        Longitude: E positive; W negative.');
+    console.log('-z            Time zone: uses tz.guess() from moment-timezone by default.');
+    console.log('-d 0-6        Day to retrieve weather: 0 is today; defaults to 1.');
+    console.log('-j            Echo pretty JSON from open-meteo API and exit.');
+
+    process.exit(0);
+}
+
+const timezone = moment.tz.guess();
+const latitude = args.n || args.s * -1;
+const longitude = args.e || args.w * -1;
+
+const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=' + latitude + '&longitude=' + longitude + '&hourly=temperature_2m&current_weather=true&timezone=' + timezone);
+const data = await response.json();
+
+if (args.j) {
+    console.log(data);
+    process.exit(0);
+} else {
+    const days = args.d;
+    console.log(data.precipitation);
+
+    if (days == 0) {
+    console.log("today.")
+    } else if (days > 1) {
+    console.log("in " + days + " days.")
+    } else {
+    console.log("tomorrow.")
+    }
+}
